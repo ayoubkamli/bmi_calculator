8998367a7568f74b69b4b50be5b7d6ff8bbde12d
ayoubkamli <ayyoub.kamli@gmail.com>
2022-03-20 22:22:51 +0100

    add some stylles and colors to the ui

---
+++ lib/screens/home.dart
@@ -0,0 +1,26 @@
+import 'package:diary/constants/app_constants.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'BMI Calculator',
+          style: TextStyle(color: accentHexColor, fontWeight: FontWeight.w300),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      backgroundColor: mainHexColor,
+      body: Column(children: []),
+    );
+  }
+}
